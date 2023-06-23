9be3104816fc37c4ce6b53a8d2137a31444fbbf0
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 11:37:13 +0800

    add user model
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+  validates :eth_address, presence: true, uniqueness: true
+  validates :eth_nonce, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+end
