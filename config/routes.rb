d76ec89e0329f1f999920f8add1dfc2608d2050f
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 22:17:56 +0800

    add session paths
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get    "users",     to: "users#index"
   get    "signup",    to: "users#new"
   post   "signup",    to: "users#create"
+
+  get    "login",  to: "sessions#new"
+  post   "login",  to: "sessions#create"
+  get    "logout", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
+  post   "logout", to: "sessions#destroy"
 end
