dd91a961cf847f773984deb3acc17ce2321c1dbb
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 21:50:39 +0800

    add users controller with new, create actions
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user and @user.username?
+      if @user.eth_address && Eth::Address.new(@user.eth_address).valid?
+        @user.eth_nonce = get_new_nonce
+        if @user.save
+          redirect_to login_path, notice: "Successfully created an account, you may now log in."
+        else
+          redirect_to login_path, alert: "Account already exists! Try to log in instead!"
+        end
+      else
+        flash.now[:alert] = "Failed to get Ethereum address or Invalid Ethereum address!"
+        render :new
+      end
+    else
+      flash.now[:alert] = "Please enter a username!"
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :eth_address)
+  end
+end
