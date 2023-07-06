809734bf1b3483175662619f449bf3642b90f663
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-07-06 17:46:22 +0800

    add prefix for user show path
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   get    "users",     to: "users#index"
-  get    "users/:id", to: "users#show"
+  get    "users/:id", to: "users#show", as: :user
   get    "signup",    to: "users#new"
   post   "signup",    to: "users#create"
 
