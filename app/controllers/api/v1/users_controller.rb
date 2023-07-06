9e8fd7a701487f80c9ef7254a8cd285e61f07e5a
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-07-06 10:30:31 +0800

    make find user step as a private function for all actions
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::UsersController < ApiController
 
   def show
-    eth_address = Eth::Address.new(params[:id])
-    user = User.find_by(eth_address: params[:id].downcase) if eth_address.valid?
-
     render json: [eth_nonce: user.eth_nonce] if user.present?
+  end
+
+  private
+
+  def user
+    eth_address = Eth::Address.new(params[:id])
+    @user ||= User.find_by(eth_address: params[:id].downcase) if eth_address.valid?
   end
 end
