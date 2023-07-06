34f0fcdfe1d372cde721af7eef1f6cb6614e9cce
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-07-06 10:38:00 +0800

    check if user is minted
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   validates :eth_address, presence: true, uniqueness: true
   validates :eth_nonce, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def is_minted?
+    tlwh = TLWH.new
+    tlwh.eth_client.call(
+      tlwh.contract,
+      'balanceOf',
+      self.eth_address
+    ) == 1
+  end
 end
