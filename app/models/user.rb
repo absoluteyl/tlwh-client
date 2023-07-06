58a0e2fa80a94e10100f2c87a3b230e036a70d99
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-07-06 10:54:38 +0800

    add generate ipfs file function
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,21 @@ class User < ApplicationRecord
       self.eth_address
     ) == 1
   end
+
+  def generate_ipfs_file
+    tlwh = TLWH.new
+    token_id = tlwh.eth_client.call(tlwh.contract, 'totalSupply') + 1
+    ipfs_data = {
+      id: token_id,
+      name: "The Last Watch Hist ##{token_id}",
+      owner: self.eth_address,
+      image: Ipfs.url(ENV['NFT_IMAGE_ID']),
+    }
+    tmp = Tempfile.new("TLWH-#{token_id}")
+    tmp.write(ipfs_data.to_json)
+    tmp.close
+
+    ipfs_client = Ipfs.client
+    ipfs_client.add(tmp.path)['Hash']
+  end
 end
