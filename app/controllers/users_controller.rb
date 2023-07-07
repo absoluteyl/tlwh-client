0a703e896fee644e834a3187764bca6904588951
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-07-07 15:34:55 +0800

    fetch user's watch hist and parse data
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    # Retrieve KKTV watch history
+    @watch_hist = generate_watch_hist if @user.kktv_user_token.present?
   end
 
   def new
@@ -51,5 +53,42 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :eth_address, :kktv_user_token)
+  end
+
+  def generate_watch_hist
+    raw_watch_hist = Kktv.client.watch_hist(@user)
+    watch_hist = []
+    raw_watch_hist.each do |hist|
+      detail = Kktv.client.title_detail(@user, hist[:title][:id])
+      item = {}
+      # General Info
+      item[:name]  = hist[:title][:name]
+      item[:cover] = detail[:cover]
+      item[:link]  = detail[:deeplink]
+
+      # Watch History
+      item[:progress] = hist[:display][:description]
+
+      # Meta
+      item[:type] = case hist[:title][:title_type]
+        when "film"
+          "Movie"
+        when "miniseries", "series"
+          "TV Series"
+        end
+      item[:year] = detail[:release_year]
+      item[:categories] = detail[:genres].to_a + detail[:themes].to_a
+      item[:categories] = item[:categories].map{ |c|
+        next if c[:collection_name].in?(['免費', '語言學習', '雙字幕'])
+        c[:collection_name]
+      }.compact.join(', ')
+
+      item[:casts] = detail[:casts].map{ |c| c[:collection_name] }.join(", ") if detail[:casts].present?
+
+      item[:crews] = detail[:directors].to_a + detail[:producers].to_a
+      item[:crews] = item[:crews].to_a.map{ |c| c[:collection_name] }.compact.join(', ')
+      watch_hist << item
+    end
+    watch_hist
   end
 end
